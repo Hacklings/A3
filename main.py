edf9e080708b210d28823851a5e065b271ecba5d
anshul-1111 <64676489+anshul-1111@users.noreply.github.com>
2022-10-24 11:37:24 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from travel import create_app
 import logging
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__=='__main__':
     n_app=create_app()
     n_app.run(debug=True)
-    app.logger.addHandler(logging.StreamHandler(sys.stdout))
-    app.logger.setLevel(logging.ERROR)
